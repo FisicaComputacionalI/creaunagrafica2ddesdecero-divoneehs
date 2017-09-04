8fe2e82dfb65dc353ad0f27c99beda09240cdbb4
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-04 17:26:35 -0500

    Add files via upload
    Hola, envío la gráfica solicitada, lo hice en Spyder pero no lo entiendo bien aún entonces no se si corra en cualquier lado, por ello, envío también la captura de pantalla con la gráfica que me resultó. Saludos :)

---
+++ Ivonee.py
@@ -0,0 +1,15 @@
+import pylab as pl
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y = [8.5,9.6,9.2,8.8]
+pl.plot(x, y, '*')
+plt.xlabel("Semestre")
+plt.ylabel("Promedio")
+plt.title("Promedio general por semestre cursado")
+pl.savefig('promedioporsemestre')
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+
